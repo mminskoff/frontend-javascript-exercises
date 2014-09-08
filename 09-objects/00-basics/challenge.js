fe3fdfcd0ae0e1fd571058c86bdcb38320ab03fb
vagrant <vagrant@precise32.(none)>
2014-09-08 18:56:35 +0000

    objects

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -13,8 +13,21 @@ module.exports.addProperty = function (object, newProp, newValue) {
 	return object;
 };
 
-module.exports.formLetter = function () {
+module.exports.formLetter = function (letter) {
+	return "Hello " + letter.recipient + ",\n\n" + letter.msg + "\n\nSincerely,\n" + letter.sender;
 };
 
-module.exports.canIGet = function () {
+module.exports.canIGet = function (item,money) {
+	var items = {
+		"MacBook Air": 999,
+		"MacBook Pro": 1299,
+		"Mac Pro": 2499,
+		"Apple Sticker": 1
+	};
+
+	price = items[item]
+	return (price !== undefined && money >= price);
+
 };
+
+//GO OVER THIS
